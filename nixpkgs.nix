83fb38bb6634529bdffbd13f3939831e19b142ae
Jasper Woudenberg <mail@jasperwoudenberg.com>
2020-10-10 07:20:11 +0100

    Update nix environment

--- nixpkgs.nix
+++ nixpkgs.nix
@@ -3,12 +3,12 @@ let
   #
   # Pick a release (e.g. nixpkgs-unstable) and open the `git-revision`
   # file. It will contain a revision hash. Copy and paste it below.
-  rev = "a8fc904c7c0d66f07d22bcb59a46d2bd72f8ddae";
+  rev = "0c59c1296b23abc25a6383ff26db2eeb17ad8a81";
   # Generate the SHA256 hash for this revision's tarball.
   #
   #   $ nix-prefetch-url --unpack --type sha256 \
   #   >   https://github.com/NixOS/nixpkgs/archive/${rev-defined-above}.tar.gz
-  sha256 = "142gzi01601c95hrwjizjc25qsmswldzw2zg6fvgw52lpag2w2qb";
+  sha256 = "03sifcpkc3prszaycd6snvpxam66phmj0b7m4723l5dmmsyq4bkw";
   nixpkgs = builtins.fetchTarball {
     url = "https://github.com/NixOS/nixpkgs/archive/${rev}.tar.gz";
     sha256 = sha256;
